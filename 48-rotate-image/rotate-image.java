884e2106a9fa1d345ede32eee0de8cf9f9a10861
Kartik. <144789279+qarteek@users.noreply.github.com>
2026-07-06 16:49:19 +0530

    Time: 0 ms (100.00%) | Memory: 43.9 MB (34.44%) - LeetSync

---
+++ 48-rotate-image/rotate-image.java
@@ -0,0 +1,29 @@
+class Solution {
+    public void rotate(int[][] arr) {
+        int n= arr.length;
+        int m= arr[0].length;
+        for (int i = 0; i < n; i++) {
+            for (int j = i+1; j < m; j++) {
+                int temp = arr[i][j];
+                arr[i][j]=arr[j][i];
+                arr[j][i]=temp;
+
+            }
+
+        }
+
+        for (int i = 0; i < n; i++) {
+            int left=0, right=n-1;
+            while (left<right){
+                int temp= arr[i][left];
+                arr[i][left]=arr[i][right];
+                arr[i][right]=temp;
+                left++;
+                right--;
+
+            }
+
+        }
+        
+    }
+}
