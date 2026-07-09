4b3f54b08c5f6621feb84d4c7e1d0d03709a4ba6
Kartik. <144789279+qarteek@users.noreply.github.com>
2026-07-09 20:19:24 +0530

    Time: 0 ms (100.00%) | Memory: 45.1 MB (46.23%) - LeetSync

---
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
@@ -0,0 +1,56 @@
+class Solution {
+    public boolean search(int[] nums, int target) {
+        int n= nums.length;
+        
+        int l= 0, h=n-1;
+        while (l <= h) {
+            int mid = l + (h - l) / 2;
+
+            if (nums[mid] == target)
+                return true;
+
+            if (nums[l] == nums[mid] && nums[mid] == nums[h]) {
+                l++;
+                h--;
+                continue;
+            }
+
+            if (nums[l] <= nums[mid]) {
+                if (nums[l] <= target && target < nums[mid])
+                    h = mid - 1;
+                else
+                    l = mid + 1;
+            } else {
+                if (nums[mid] < target && target <= nums[h])
+                    l = mid + 1;
+                else
+                    h = mid - 1;
+            }
+        }
+        // while(l<h){
+        //     int mid = (l+h)/2;
+        //     if (nums[l] == nums[mid] && nums[mid] == nums[h]) {
+        //                     l++;
+        //                     h--;}
+        //     if(nums[mid]==target) return true;
+        //     if (nums[l] == nums[mid]) {
+        //         l++;
+                
+        //         continue;}
+
+        //     if(nums[l]<nums[mid]){
+        //         if(nums[l]<=target && target< nums[mid]){
+        //             h=mid-1;}
+        //             else l=mid+1;
+        //         }
+        //         else {
+        //             if(nums[mid]<target && target<=nums[h]){
+        //                 l=mid+1;
+        //             }
+        //             else h=mid-1;
+        //         }
+            return false;
+        }
+        
+        
+}
